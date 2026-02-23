5902a9275fa2919c71a8c981677e33946f500fb2
ddux <dusan.stanojevic.cs@gmail.com>
2026-02-22 22:06:57 -0600

    proper main

--- backend/cmd/traceway/main.go
+++ backend/cmd/traceway/main.go
@@ -3,10 +3,11 @@ package main
 import tracewaybackend "github.com/tracewayapp/traceway/backend"
 
 func main() {
-	tracewaybackend.Run(
-		tracewaybackend.WithPort(8082),
-		tracewaybackend.WithDefaultUser("admin@localhost.com", "admin"),
-		tracewaybackend.WithDefaultProject("Backend", "go", "backend-dev-token"),
-		tracewaybackend.WithDefaultProject("Frontend", "sveltekit", "frontend-dev-token"),
-	)
+	tracewaybackend.Run()
+	// tracewaybackend.Run(
+	// 	tracewaybackend.WithPort(8082),
+	// 	tracewaybackend.WithDefaultUser("admin@localhost.com", "admin"),
+	// 	tracewaybackend.WithDefaultProject("Backend", "go", "backend-dev-token"),
+	// 	tracewaybackend.WithDefaultProject("Frontend", "sveltekit", "frontend-dev-token"),
+	// )
 }
